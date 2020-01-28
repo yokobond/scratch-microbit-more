2d5eb8cd23151439ba9d8c7b8bfd4106da704f80
yokobond <koji.yokokawa@gmail.com>
2020-01-28 10:36:13 +0900

    Add a script to setup develop environment.

---
+++ gui_ext_index-code.jsx
@@ -0,0 +1,41 @@
+// <icon>
+import mbitMoreIconURL from './mbitMore/mbitMore.png';
+import mbitMoreInsetIconURL from './mbitMore/mbitMore-small.svg';
+import mbitMoreConnectionIconURL from './mbitMore/mbitMore-illustration.svg';
+import mbitMoreConnectionSmallIconURL from './mbitMore/mbitMore-small.svg';
+// </icon>
+
+[
+    // <configuration>
+    {
+        name: 'Mbit More',
+        extensionId: 'mbitMore',
+        collaborator: 'Yengawa Lab',
+        iconURL: mbitMoreIconURL,
+        insetIconURL: mbitMoreInsetIconURL,
+        description: (
+            <FormattedMessage
+                defaultMessage="Connect your projects with the world."
+                description="Description for the 'micro:bit' extension"
+                id="gui.extension.microbit.description"
+            />
+        ),
+        featured: true,
+        disabled: false,
+        bluetoothRequired: true,
+        internetConnectionRequired: false,
+        launchPeripheralConnectionFlow: true,
+        useAutoScan: false,
+        connectionIconURL: mbitMoreConnectionIconURL,
+        connectionSmallIconURL: mbitMoreConnectionSmallIconURL,
+        connectingMessage: (
+            <FormattedMessage
+                defaultMessage="Connecting"
+                description="Message to help people connect to their micro:bit."
+                id="gui.extension.microbit.connectingMessage"
+            />
+        ),
+        helpLink: 'https://lab.yengawa.com/project/scratch-microbit-more/'
+    },
+    // </configuration>
+]
---
+++ setup-dev.js
@@ -0,0 +1,55 @@
+const path = require('path');
+const fs = require('fs');
+
+const VmRoot = '../scratch-vm';
+const GuiRoot = '../scratch-gui';
+
+const ExtId = 'mbitMore';
+const VmExtDirName = 'mbitMore';
+
+const VmExtPath = 'src/extensions/' + VmExtDirName;
+const GuiExtPath = 'src/lib/libraries/extensions/' + ExtId;
+const VmExtManager = 'src/extension-support/extension-manager.js'
+const GuiExtIndex = 'src/lib/libraries/extensions/index.jsx'
+const GuiExtIndexConfig = fs.readFileSync('./gui_ext_index-code.jsx', 'utf-8');
+
+// Make symbolic link in scratch-vm. 
+try {
+    fs.symlinkSync(path.resolve(path.join('./scratch-vm', VmExtPath)), path.resolve(path.join(VmRoot, VmExtPath)));
+    console.log(`Make link: ${path.resolve(path.join(VmRoot, VmExtPath))}`);
+} catch (err) {
+    console.log(`Already exists link: ${path.resolve(path.join(VmRoot, VmExtPath))}`);
+};
+
+// Add the extension to extension manager of scratch-vm. 
+let managerCode = fs.readFileSync(path.resolve(path.join(VmRoot, VmExtManager)), 'utf-8');
+if (managerCode.includes(ExtId)) {
+    console.log(`Already registered in manager: ${ExtId}`);
+} else {
+    fs.copyFileSync(path.resolve(path.join(VmRoot, VmExtManager)), path.resolve(path.join(VmRoot, VmExtManager + '_orig')));
+    managerCode = managerCode.replace(/builtinExtensions = {[\s\S]*?};/, `$&\n\nbuiltinExtensions.${ExtId} = () => require('../extensions/${VmExtDirName}');`);
+    fs.writeFileSync(path.resolve(path.join(VmRoot, VmExtManager)), managerCode);
+    console.log(`Registered in manager: ${ExtId}`);
+}
+
+// Make symbolic link in scratch-gui. 
+try {
+    fs.symlinkSync(path.resolve(path.join('./scratch-gui', GuiExtPath)), path.resolve(path.join(GuiRoot, GuiExtPath)));
+    console.log(`Make link: ${path.resolve(path.join(GuiRoot, GuiExtPath))}`);
+} catch (err) {
+    console.log(`Already exists link: ${path.resolve(path.join(GuiRoot, GuiExtPath))}`);
+};
+
+// Add the extension to list of scratch-gui. 
+let indexCode = fs.readFileSync(path.resolve(path.join(GuiRoot, GuiExtIndex)), 'utf-8');
+if (indexCode.includes(ExtId)) {
+    console.log(`Already added to extrnsion list: ${ExtId}`);
+} else {
+    fs.copyFileSync(path.resolve(path.join(GuiRoot, GuiExtIndex)), path.resolve(path.join(GuiRoot, GuiExtIndex + '_orig')));
+    indexCode = indexCode.replace(/^.*export default\s+\[\s*$/m,
+        GuiExtIndexConfig.match(/<icon>.*[\r\n]+([\s\S]*)$[\r\n].*<\/icon>/mi)[1]
+        + '\n\n$&\n'
+        + GuiExtIndexConfig.match(/<configuration>.*[\r\n]+([\s\S]*)$[\r\n].*<\/configuration>/mi)[1]);
+    fs.writeFileSync(path.resolve(path.join(GuiRoot, GuiExtIndex)), indexCode);
+    console.log(`Added to extrnsion list: ${ExtId}`);
+}
