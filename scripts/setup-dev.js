2970882401b281516cecd86c09e43491b3b9b35a
yokobond <koji.yokokawa@gmail.com>
2020-09-25 14:25:29 +0900

    Remove install process from setup-dev script.

--- scripts/setup-dev.js
+++ scripts/setup-dev.js
@@ -1,12 +1,8 @@
 const path = require('path');
 const fs = require('fs');
-const { execSync } = require('child_process')
 
-const ExtRoot = path.resolve(__dirname, '../');
 const VmRoot = path.resolve(__dirname, '../../scratch-vm');
 const GuiRoot = path.resolve(__dirname, '../../scratch-gui');
-
-let stdout;
 
 // Use local scratch-vm in scratch-gui
 try {
@@ -22,27 +18,3 @@ try {
 } catch (err) {
     console.log(err);
 }
-
-// Install this extension.
-try {
-    stdout = execSync(`cd ${ExtRoot} && node install.js --gui=${GuiRoot}`);
-    console.log(`stdout: ${stdout.toString()}`);
-} catch (err) {
-    console.error(err);
-}
-
-
-// // Setup for ESLint.
-// try {
-//     fs.symlinkSync(path.resolve(path.join(VmRoot, 'node_modules')), path.resolve(path.join(ExtRoot, 'scratch-vm', 'node_modules')));
-//     console.log(`Make link: ${path.resolve(path.join(ExtRoot, 'scratch-vm', 'node_modules'))}`);
-// } catch (err) {
-//     console.log(`Already exists link: ${path.resolve(path.join(ExtRoot, 'scratch-vm', 'node_modules'))}`);
-// }
-
-// try {
-//     fs.symlinkSync(path.resolve(path.join(VmRoot, 'src', '.eslintrc.js')), path.resolve(path.join(ExtRoot, 'scratch-vm', 'src', '.eslintrc.js')));
-//     console.log(`Make link: ${path.resolve(path.join(ExtRoot, 'scratch-vm', 'src', '.eslintrc.js'))}`);
-// } catch (err) {
-//     console.log(`Already exists link: ${path.resolve(path.join(ExtRoot, 'scratch-vm', 'src', '.eslintrc.js'))}`);
-// }
