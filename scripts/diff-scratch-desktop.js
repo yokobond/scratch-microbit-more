1967db2e2b8a24b09f574da2a83c8b9cf0816528
yokobond <koji.yokokawa@gmail.com>
2020-09-27 18:15:49 +0900

    Move script file.

--- scripts/diff-scratch-desktop.js
+++ scripts/diff-scratch-desktop.js
@@ -1,8 +1,8 @@
 const path = require('path');
 const { execSync } = require('child_process');
 
-const ExtRoot = path.resolve(__dirname);
-const DesktopRoot = path.resolve(__dirname, '../scratch-desktop');
+const ExtRoot = path.resolve(__dirname, '../');
+const DesktopRoot = path.resolve(__dirname, '../../scratch-desktop');
 
 try {
     execSync(`cd ${DesktopRoot} && git diff -- .  ':(exclude)package-lock.json' ':(exclude)buildResources' ':(exclude)src/icon' > ${path.join(ExtRoot, 'scripts', 'scratch-desktop.patch')}`);
