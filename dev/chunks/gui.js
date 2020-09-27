bf9aa8ada4679f5e899b48dbc338602c49855d6f
yokobond <yokobond@users.noreply.github.com>
2020-09-27 09:41:25 +0000

    deploy: 352d94a7feaa2ba477b7b3c506ac2d87aa840116

--- dev/chunks/gui.js
+++ dev/chunks/gui.js
@@ -147,7 +147,7 @@ function _typeof(obj) { "@babel/helpers - typeof"; if (typeof Symbol === "functi
 
 
 var onClickLogo = function onClickLogo() {
-  window.location = 'https://lab.yengawa.com/project/scratch-microbit-more/';
+  window.location = 'https://scratch.mit.edu';
 };
 
 var handleTelemetryModalCancel = function handleTelemetryModalCancel() {
