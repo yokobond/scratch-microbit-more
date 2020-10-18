2cb87806fe2ff1a9c9e55442dce147e492391c4f
yokobond <koji.yokokawa@gmail.com>
2020-10-18 20:03:50 +0900

    Fix to work stretch3 installer.

--- install-stretch3.sh
+++ install-stretch3.sh
@@ -9,18 +9,18 @@ LF=$(printf '\\\012_')
 LF=${LF%_}
 
 mkdir -p node_modules/scratch-vm/src/extensions/microbitMore
-cp microbitMore/scratch-vm/src/extensions/microbitMore/index.js node_modules/scratch-vm/src/extensions/microbitMore/
+cp microbitMore/src/block/index.js node_modules/scratch-vm/src/extensions/microbitMore/
 mv node_modules/scratch-vm/src/extension-support/extension-manager.js node_modules/scratch-vm/src/extension-support/extension-manager.js_orig
 sed -e "s|class ExtensionManager {$|builtinExtensions['microbitMore'] = () => require('../extensions/microbitMore');${LF}${LF}class ExtensionManager {|g" node_modules/scratch-vm/src/extension-support/extension-manager.js_orig > node_modules/scratch-vm/src/extension-support/extension-manager.js
 
 mkdir -p src/lib/libraries/extensions/microbitMore
-cp microbitMore/scratch-gui/src/lib/libraries/extensions/microbitMore/microbitMore.png src/lib/libraries/extensions/microbitMore/
-cp microbitMore/scratch-gui/src/lib/libraries/extensions/microbitMore/microbitMore-small.svg src/lib/libraries/extensions/microbitMore/
-cp microbitMore/scratch-gui/src/lib/libraries/extensions/microbitMore/microbitMore-illustration.svg src/lib/libraries/extensions/microbitMore/
+cp microbitMore/src/entry/microbitMore.png src/lib/libraries/extensions/microbitMore/
+cp microbitMore/src/entry/microbitMore-small.svg src/lib/libraries/extensions/microbitMore/
+cp microbitMore/src/entry/microbitMore-illustration.svg src/lib/libraries/extensions/microbitMore/
 mv src/lib/libraries/extensions/index.jsx src/lib/libraries/extensions/index.jsx_orig
 MICROBIT_MORE="\
     {${LF}\
-        name: 'micro:bit MORE v0.4.3',${LF}\
+        name: 'micro:bit MORE v0.5.0',${LF}\
         extensionId: 'microbitMore',${LF}\
         collaborator: 'Yengawa Lab',${LF}\
         iconURL: microbitMoreIconURL,${LF}\
